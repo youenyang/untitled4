ce041bf3f1f673d160434ef28695bfb40d84c102
zl <984822673@qq.com>
2021-01-20 19:29:08 +0800

    第一次提交

--- src/main/java/a/a.java
+++ src/main/java/a/a.java
@@ -4,4 +4,8 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class a {
     //aaaaa
+    ///
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
 }
